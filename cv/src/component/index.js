621edf06f09e196ae3848de16234420219dfe389
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-01 19:54:35 +0300

    update

---
+++ cv/src/component/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import style from './style.css'
+export default class index extends Component {
+    render() {
+        return (
+            <div className = 'first1'>
+              <div className = 'first'> <h1>ِAbout</h1>
+      <p>Since 10 years ago i just think i have to learn Securit Lorem ipsum dolor sit amet
+         consectetur adipisicing elit. Odit commodi distinctio adipisci, minus libero nulla quis numquam nisi <br/> 
+        quaerat explicabo maiores neque repudiandae repellendus debitis quibusdam facilis ducimus dolores voluptatum. </p> 
+        <hr/><h1>Work Experince</h1>
+        <p>Since 10 years ago i just think i have to learn Securit Lorem ipsum dolor sit amet
+         consectetur adipisicing elit. Odit commodi distinctio adipisci, minus libero nulla quis numquam nisi <br/> 
+        quaerat explicabo maiores neque repudiandae repellendus debitis quibusdam facilis ducimus dolores voluptatum.</p>
+
+      <hr/>  <h1>Skills</h1>
+        <p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p>
+        <h1>Education</h1><p>Bachelor Degree From Alqassim Univ</p>
+   </div> <div className = 'image'> <img src="https://i.ibb.co/wyPPZXw/sultan.jpg" alt="sultan" border="0"/>
+
+
+<p>Sultan Alharbi</p><br/><p>Programmer under training</p></div>       
+            </div>
+            
+        )
+    }
+}
