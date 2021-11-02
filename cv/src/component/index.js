6fd4f451e0d4db0acb3bc20227efd75f4814fe0a
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-11-02 11:46:00 +0300

    lastupdate

--- cv/src/component/index.js
+++ cv/src/component/index.js
@@ -14,9 +14,9 @@ export default class index extends Component {
         quaerat explicabo maiores neque repudiandae repellendus debitis quibusdam facilis ducimus dolores voluptatum.</p>
 
       <hr/>  <h1>Skills</h1>
-        <p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p><p>fff</p><p>ggg</p>
+        <p>HTML</p><p>CSS</p><p>JAVASCRIPT</p><p>REACT</p><p>ES6</p><p>PROBLEM SOLVING</p><p>KALI</p><p>Node.js</p><p>Github</p><p>CL</p>
         <h1>Education</h1><p>Bachelor Degree From Alqassim Univ</p>
-   </div> <div className = 'image'> <img src="https://i.ibb.co/wyPPZXw/sultan.jpg" alt="sultan" border="0"/>
+   </div> <div className = 'image'> <img src="https://i.ibb.co/9q3R0h9/r0x.jpg" alt="sultan" border="0"/>
 
 
 <p>Sultan Alharbi</p><br/><p>Programmer under training</p></div>       
